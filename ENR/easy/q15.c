2a68d84e6c3eb6fa120d73e117e5b4f6f92f8d07
fenil <fenilp2046@gmail.com>
2024-11-02 01:42:42 +0530

    added successfully

---
+++ ENR/easy/q15.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int n1,count=0;
+
+    printf("Enter a number : ");
+    scanf("%d",&n1);
+
+    for(int i=2;i<n1;i++){
+        if(n1%i==0){
+            count++;
+        }
+       
+    }
+
+    if(count>0){
+        printf("%d is not prime",n1);
+    }
+    else{
+        printf("%d is  prime",n1);
+    }
+
+
+return 0;
+}
