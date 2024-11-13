cdac21ec0095dafd0cf212cec7299b29b2139aca
fenil <fenilp2046@gmail.com>
2024-11-14 01:26:02 +0530

    added successfully

--- ENR/medium/q10.c
+++ ENR/medium/q10.c
@@ -2,16 +2,11 @@
 
 int main() {
     int a, b;
-
-   
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
-
-    a = a + b;  
-    b = a - b;   
-    a = a - b;                                                                                                                  
-
-
+    a=a+b;  
+    b=a-b;   
+    a=a-b;                                                                                                                  
     printf("a = %d, b = %d\n", a, b);
 
     return 0;
